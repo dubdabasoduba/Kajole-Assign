d6330035edfc1ef252a0fc42542aca3c9a5c67d9
dubdabasoduba <dubdabasoduba@gmail.com>
2014-07-28 10:13:51 +0300

    correction

--- src/Assign/Assign1.java
+++ src/Assign/Assign1.java
@@ -2,6 +2,7 @@
  * Program to promt user and draw a rectangle with user specification
  */
 package Assign;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Scanner;
@@ -9,18 +10,19 @@ import java.util.Scanner;
 class MyCanvas extends JComponent {
 
 	public void paint(Graphics g) {
-		Scanner user_input = new Scanner( System.in );
-		int firstvalue,secondvalue;
+		Scanner user_input = new Scanner(System.in);
+		int firstvalue, secondvalue;
 		System.out.print("Please Enter Your first value");
 		firstvalue = Integer.parseInt(user_input.next());
 
 		System.out.print("Please enter your second value");
 		secondvalue = Integer.parseInt(user_input.next());
 
-		g.drawRect (10, 10, firstvalue, secondvalue);
+		g.drawRect(10, 10, firstvalue, secondvalue);
 	}
 }
-public class Assign1 extends JComponent{
+
+public class Assign1 extends JComponent {
 	public static void main(String[] a) {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/Assign/Assign2Applet.java
+++ src/Assign/Assign2Applet.java
@@ -1,7 +1,22 @@
 package Assign;
 
-/**
- * Created by benjamin on 7/26/14.
- */
-public class Assign2Applet {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Assign2Applet extends Applet {
+	public void paint (Graphics g) {
+		g.setColor(Color.blue);
+		g.drawLine(10, 10, 34, 70);
+		g.drawRect(80, 10, 30, 80);
+		g.drawOval(120, 10, 70, 70);
+		g.drawArc(200, 10, 90, 90,40, 270);
+
+		g.setColor(Color.blue);
+		g.drawRect(80, 120, 30, 80);
+		g.fillRect(80,120,30,80);
+		g.drawOval(120, 120, 70, 70);
+		g.fillOval(120,120,70,70);
+		g.drawArc(200, 120, 90, 90,40, 270);
+		g.fillArc(200,120,90,90,40,270);
+	}
 }
